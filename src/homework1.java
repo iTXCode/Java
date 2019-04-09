f4556a04cb0dd2a61efb69a5578bc6b92847b9b9
jack-wang128801 <1632672310@qq.com>
2019-04-09 23:41:51 +0800

    今天写了几个java程序，作业内容比较丰富啊，引用了类的概念实现了很多功能。 感觉c和Java的差异不是很大。

---
+++ src/homework1.java
@@ -0,0 +1,31 @@
+/*import java.util.Scanner;
+public class homework1 {
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        int i=0;
+        System.out.print("请输入学生的个数:");
+        int num=input.nextInt();
+        double arr[]=new double[num];
+        for(i=0;i<num;i++){
+            arr[i]=input.nextDouble();
+        }
+        int Score1=average1(arr);
+        double Score2=average2(arr);
+        System.out.println("整形平均成绩="+Score1);
+        System.out.println("浮点型形平均成绩="+Score2);
+    }
+    public static int average1(double []arr){
+       int sum=0;
+       for(int i=0;i<arr.length;i++){
+           sum+=arr[i];
+       }
+       return sum/arr.length;
+    }
+    public static double average2(double []arr){
+       double sum=0;
+       for(int i=0;i<arr.length;i++)
+           sum+=arr[i];
+        return sum/arr.length;
+    }
+
+}*/
---
+++ src/homework2.java
@@ -0,0 +1,25 @@
+/*import  java.util.Scanner;
+public class homework2{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("请输入数组的规模:");
+        int num=input.nextInt();
+        System.out.print("请输入数组的值:");
+        int arr[]=new int[num];
+        for(int i=0;i<num;i++) {
+        arr[i]=input.nextInt();
+        }
+        int min=Min(arr);
+        System.out.println("你所输入的数组的最小值="+min);
+
+    }
+    public static int Min(int arr[]){
+       int min=arr[0];
+       for(int i=1;i<arr.length;i++){
+           if(min>arr[i])
+               min=arr[i];
+       }
+       return min;
+    }
+}*/
+
